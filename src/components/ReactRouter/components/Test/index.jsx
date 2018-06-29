b5c562a6e43a3ffc453e0f4e8df49ed9de3f91ce
qqxu <qqxu10@126.com>
2018-06-29 09:50:05 +0800

    router for different  component

---
+++ src/components/ReactRouter/components/Test/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Test extends Component {
+  render() {
+    return (
+      <div className="About-container">
+        11111
+      </div>
+    );
+  }
+}
--- src/components/ReactRouter/index.jsx
+++ src/components/ReactRouter/index.jsx
@@ -2,6 +2,7 @@ import createHistory from 'history/createHashHistory'
 import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 import About from './components/About';
+import Test from './components/Test';
 
 export const history = createHistory()
 
@@ -9,7 +10,10 @@ export default class ReactRouter extends Component {
   render() {
     return (
       <Router history={history}>
-        <Route path="/" component={About} />
+        <switch>
+          <Route path="/" component={About} exact />
+          <Route path="/test" component={Test} exact />
+        </switch>
       </Router>
     );
   }
